a6355b6425c121dbfee7fc22b9957ac91fb578d0
rohitAniLabs <rohit@anilabsinc.com>
2023-04-17 14:29:08 +0530

    serverless-architechture

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world updated</h1>
-    </div>
-  );
-}
+import React, {useState} from 'react'
 
-export default App;
+export default App = () =>{
+  const [message,setMessage] = useState('');
+  
+  const handleClick = async()=>{
+    const response = await fetch('/api/hello');
+    const data = await response.json();
+    setMessage(data.message);
+  }
+  return(
+    <div className='App'>
+      <button onClick={handleClick}>Click Me</button>
+      <h>{message}</h>
+    </div>
+  )
+}
